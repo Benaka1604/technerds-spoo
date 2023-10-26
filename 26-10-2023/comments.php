414843d04cfac14d4945309157d4dfc22e11d0b8
Benaka <channakeshavas16@gmail.com>
2023-10-26 16:22:55 +0530

    first commit

---
+++ 26-10-2023/comments.php
@@ -0,0 +1,11 @@
+<?php
+// this is singal line comment
+//echo,$a"<br>;
+
+$a="technerds";
+echo(" $a<br>");
+print"$a<br>";
+print"technerds"."<br>".$a;
+print"technerds"."<br>".$a;
+echo("technerds"."<br>".$a);
+?>
